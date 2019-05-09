11d2659f29a9bd6a5e361bf8dfba712842be9133
Max Smirnov <smirnov.m4ksim@gmail.com>
2019-05-09 14:43:04 +0300

    configured core

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+
+class App extends Component {
+
+  render() {
+    return <div>aviasales</div>;
+  }
+}
+
+export default hot(App);
